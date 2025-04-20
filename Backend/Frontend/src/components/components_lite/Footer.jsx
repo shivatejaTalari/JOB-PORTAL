af566e51ae1535661dbc7bb6260d498daefdd0bf
shivatej <you@example.com>
2025-04-21 00:49:16 +0530

    Initial commit

---
+++ Backend/Frontend/src/components/components_lite/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+      {/* Footer for the current page */}
+      <div
+        style={{
+          textAlign: "center",
+          padding: "20px",
+          backgroundColor: "#f1f1f1",
+        }}
+      >
+        
+        
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
