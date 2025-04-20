b02b795db1e9cd2413c85b05f94a76c2b3408ec7
shivatej <you@example.com>
2025-04-21 02:04:50 +0530

    removed recent commit

--- Backend/middleware/isAuthenticated.js
+++ Backend/middleware/isAuthenticated.js
@@ -3,23 +3,21 @@ import jwt from "jsonwebtoken";
 const authenticateToken = (req, res, next) => {
   try {
     const token = req.cookies.token;
-
     if (!token) {
-      return res.status(401).json({
-        message: "No token provided",
-        success: false,
-      });
+      return res
+        .status(401)
+        .json({ message: "No token provided", success: false });
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
+    const decoded =   jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return (
+        res.status(401).json({ message: "Invalid token" }), (success = false)
+      );
+    }
     req.id = decoded.userId;
     next();
   } catch (error) {
-    return res.status(401).json({
-      message: "Invalid token",
-      success: false,
-    });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
