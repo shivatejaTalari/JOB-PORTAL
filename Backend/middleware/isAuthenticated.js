351c88d80206724e4cda2fe3c3268aa250b983db
shivatej <you@example.com>
2025-04-21 01:49:30 +0530

    test 401 error

--- Backend/middleware/isAuthenticated.js
+++ Backend/middleware/isAuthenticated.js
@@ -3,21 +3,23 @@ import jwt from "jsonwebtoken";
 const authenticateToken = (req, res, next) => {
   try {
     const token = req.cookies.token;
+
     if (!token) {
-      return res
-        .status(401)
-        .json({ message: "No token provided", success: false });
+      return res.status(401).json({
+        message: "No token provided",
+        success: false,
+      });
     }
-    const decoded =   jwt.verify(token, process.env.JWT_SECRET);
-    if (!decoded) {
-      return (
-        res.status(401).json({ message: "Invalid token" }), (success = false)
-      );
-    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.id = decoded.userId;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({
+      message: "Invalid token",
+      success: false,
+    });
   }
 };
 
